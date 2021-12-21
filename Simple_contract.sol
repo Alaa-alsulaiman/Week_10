4a6a33ab8ef86ca84592a24a92e201040d2a8451
Alaa-alsulaiman <92013091+Alaa-alsulaiman@users.noreply.github.com>
2021-12-21 03:36:56 +0300

    Create Simple_contract.sol

---
+++ Simple_contract.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0<0.9.0;
+
+contract BankAccount {}
+
+
+contract MyBank {
+    string public name;
+
+    constructor (string memory name) {
+        name = name;
+    } 
+
+
+}
+
+
+contract Bank {
+    mapping (address=>uint) account_balances;
+    function transfer(uint amount, address acctToTransferTo) external {
+        account_balances[msg.sender] -=amount;
+        account_balances[acctToTransferTo] += amount;
+
+}
+function withdraw (uint amount) external{
+
+    account_balances[msg.sender] -= amount;
+    payable(msg.sender).transfer(amount);
+}
+ 
+
+receive () external payable {
+
+    account_balances[msg.sender] += msg.value;
+}
+
+}
